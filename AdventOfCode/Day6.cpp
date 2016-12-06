bdd04ec740219b37d1f07473f353b3fddf850801
Will Hughes <willhughes091@gmail.com>
2016-12-06 09:19:37 +0000

    Add day 6 solution

---
+++ AdventOfCode/Day6.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <iterator>
+#include "InputUtils.h"
+
+using namespace std;
+
+constexpr int ALPHABET_SIZE = 26;
+
+string day6Solution()
+{
+    ifstream inputFile;
+    inputFile.open("day6_input.txt");
+
+    vector<string> lines = InputUtils::readLines(inputFile);
+    vector<array<int, ALPHABET_SIZE>> frequencies(lines[0].length(), { 0 });
+    vector<char> mostCommonCharacter(lines[0].length(), 'a');
+
+    for (string& line : lines)
+    {
+        for (unsigned int i = 0; i < line.length(); i++)
+        {
+            int& characterFrequencyForColumn = frequencies[i][line[i] - 'a'];
+            int& mostCommonCharacterFrequencyForColumn = frequencies[i][mostCommonCharacter[i] - 'a'];
+
+            characterFrequencyForColumn++;
+
+            if (characterFrequencyForColumn > mostCommonCharacterFrequencyForColumn)
+            {
+                mostCommonCharacter[i] = line[i];
+            }
+        }
+    }
+
+    return { mostCommonCharacter.begin(), mostCommonCharacter.end() };
+}
--- AdventOfCode/Main.cpp
+++ AdventOfCode/Main.cpp
@@ -7,17 +7,19 @@
 #include "Day3.h"
 #include "Day4.h"
 #include "Day5.h"
+#include "Day6.h"
 
 using namespace std;
 
-constexpr int DEFAULT_DAY = 5;
+constexpr int DEFAULT_DAY = 6;
 const unordered_map<int, function<string ()>> DAY_SOLUTIONS
 {
     { 1, day1Solution },
     { 2, day2Solution },
     { 3, day3Solution },
     { 4, day4Solution },
-    { 5, day5Solution }
+    { 5, day5Solution },
+    { 6, day6Solution }
 };
 
 int main(int argc, char* argv[])
